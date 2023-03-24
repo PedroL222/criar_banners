b22e84d86268803c24a25fdb1e3c005225627436
Pedro Lucas Freitas Sousa <Pedrofreitas16083@gmail.com>
2023-03-24 14:46:04 -0300

    Mostrar banners criado

--- view/criar_banner.php
+++ view/criar_banner.php
@@ -57,19 +57,14 @@ if(!isset($_SESSION['id']) && !isset($_SESSION['nome'] )){
         <nav aria-label="breadcrumb">
           <div style="display:flex; align-items: center;">
             <h3><a class="opacity-5 text-dark" href="./criar_banner.php">Criar Banner</a></h3>------
-            <h3><a class="opacity-5 text-dark" href="./criar_menu.php">Criar Menu</a></h3>
+            <h3><a class="opacity-5 text-dark" href="./criar_menu.php">Criar Menu</a></h3>------
+            <h3><a class="opacity-5 text-dark" href="./mostrarbanner.php">Mostrar seus Banners</a></h3>
+            
             </div>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-            <div class="input-group">
-              <span class="input-group-text text-body bg-white  border-end-0 ">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                </svg>
-              </span>
-              <input type="text" class="form-control ps-0" placeholder="Search">
-            </div>
+            
           </div>
           <ul class="navbar-nav  justify-content-end">
             <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
@@ -193,7 +188,7 @@ if(!isset($_SESSION['id']) && !isset($_SESSION['nome'] )){
       <label for="inputtext5" class="form-label"><h3>Texto do Banner</h3></label>
       <input name="texto_ban" type="text" class="form-control">
 
-      <label for="img_ban" class="form-label"><h3>Texto do Banner</h3></label><br>
+      <label for="img_ban" class="form-label"><h3>Imagem do Banner</h3></label><br>
       <input name="arquivo" type="file" class="form-control">
 
       <input name="id" value="<?php echo $id; ?>" style="display: none;">
---
+++ view/mostrarbanner.php
@@ -0,0 +1,53 @@
+<?php
+
+session_start();
+ob_start();
+
+
+if(!isset($_SESSION['id']) && !isset($_SESSION['nome'] )){
+    header('Location: ../index.php');
+    $_SESSION['msg'] = '<p>Erro: Você tem que está logado para acessar o site</p>';
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link id="pagestyle" href="../assets/css/corporate-ui-dashboard.css?v=1.0.0" rel="stylesheet" />
+    <link rel="stylesheet" href="../css_normal/mostrar_banner.css">
+</head>
+<body>
+
+        <?php
+
+            include_once("../config.php");
+            $id = $_SESSION['id'];
+
+            $sql2 = "SELECT * FROM T_banner WHERE T_usuario_idT_usuario = $id";
+            $exec2 = $dbh->prepare($sql2);
+            $res2 = $exec2->execute();
+            $itens = $exec2->fetchAll();
+
+            foreach($itens as $linhas){
+
+        ?>
+
+    <div id="banner">
+    <h1 id="banner-text"><?php echo $linhas['texto_ban'] ?></h1>
+    <a id="banner-link" href="<?php echo $linhas['link_ban'] ?>">
+    </a>
+    <img id="banner-image" src="<?php echo $linhas['img_ban'] ?>" alt="Imagem do Banner" />
+    </div>
+
+    <?php } ?>
+
+    <div style="width: 100%; display:flex; justify-content: center; margin-top:20px;"><a href="http://localhost/criar_banners/view/criar_banner.php" type="submit" class="btn btn-primary">Voltar</a></div>
+    
+</body>
+</html>
